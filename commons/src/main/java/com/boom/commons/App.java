37b236c5b9c9c7b38dcf758d2c612332b7110c3f
hmanon <hmail@list.ru>
2012-06-14 00:42:21 +0400

    cp

---
+++ commons/src/main/java/com/boom/commons/App.java
@@ -0,0 +1,13 @@
+package com.boom.commons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
